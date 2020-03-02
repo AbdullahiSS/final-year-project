80daf525f153e17db9401eaa5b6076cd61266c8d
AbdullahiSS <asalehshehu@gmail.com>
2020-03-02 16:57:00 +0000

    Integrated the google maps api

--- src/app/main/navigation/navigation.component.ts
+++ src/app/main/navigation/navigation.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'main-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.sass']
 })
-export class NavigationComponent implements OnInit {
+
+
+
+export class NavigationComponent implements AfterViewInit {
+
+  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+  map: google.maps.Map;
+  lat = 40.730610;
+  lng = -73.935242;
+
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8
+  };
+  //Marker
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map,
+  });
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement,
+      this.mapOptions);
+      this.marker.setMap(this.map);
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
